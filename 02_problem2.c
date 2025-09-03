f6a91db7dce575f4664642cd5dbd2e54af98cfa5
Ishwarya S <sureshishwarya666@gmail.com>
2025-09-03 23:10:16 +0530

    Update 02_problem2.c

--- 02_problem2.c
+++ 02_problem2.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h>// checking divisibility
 int main(){
     int a =33950;
-    printf("The value of a is %d",a%97);
+    printf("The value of a is %d",a%97); //the value given is divisble by 97 and hence the output value will be 0.
     return 0;
 }
